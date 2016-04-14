1c7cd111df0a8921a5fbab6d6289f2025b3852e6
Humphrey <hum3@drummond.info>
2016-04-14 14:46:45 +0100

    Removing scripts

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup_dict = dict(
         'py',
     ],
     # console=['scripts/piecash_ledger.py','scripts/piecash_toqif.py'],
-    scripts=['scripts/piecash_ledger.py','scripts/piecash_toqif.py','scripts/piecash_prices.py'],
+    scripts=[],
     cmdclass = {'test': PyTest},
     test_suite="tests",
     zip_safe=False,  # don't use eggs
