fa50550b4fc0d62c833b96a04d7cbd36dccf6912
Humphrey <hum3@drummond.info>
2016-04-14 14:38:10 +0100

    Make sure can read README file.

--- setup.py
+++ setup.py
@@ -22,6 +22,18 @@ PYTEST_FLAGS = ['--doctest-modules']
 python_version_specific_requires = []
 
 
+def read(filename):
+    """Return the contents of a file.
+
+    :param filename: file path
+    :type filename: :class:`str`
+    :return: the file's content
+    :rtype: :class:`str`
+    """
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
+
+
 # Import metadata. Normally this would just be:
 #
 # from luca import metadata
