5706da6c0787f094239ca45aeec933f2d25eaa3f
Humphrey <hum3@drummond.info>
2016-04-14 14:43:37 +0100

    Adding PyTest reference

--- setup.py
+++ setup.py
@@ -8,7 +8,25 @@ import subprocess
 import platform
 
 from setuptools import setup, find_packages, Command
-# from setuptools.command.test import test as TestCommand
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        # self.test_args = []
+        # self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 # Add the current directory to the module search path.
 sys.path.append('.')
