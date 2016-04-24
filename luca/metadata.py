0265bf6f8f631245fc83c78a2b2a0591f02d975f
Humphrey Drummond <hum3@drummond.info>
2016-04-24 10:00:04 +0100

    oops didn't finish editing of excel report nor run tests

--- luca/metadata.py
+++ luca/metadata.py
@@ -7,7 +7,7 @@ Information describing the project.
 package = 'luca'
 project = "Python Trial Balance Accounting"
 project_no_spaces = project.replace(' ', '')
-version = '0.0.16'
+version = '0.0.17'
 description = 'A pythonic interface Accounting Journal Entries and .'
 authors = ['drummonds']
 authors_string = ', '.join(authors)
