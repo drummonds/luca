237c9d7c7949f680073ba3d4ad704b08d94fcbbe
Humphrey Drummond <hum3@drummond.info>
2016-05-06 17:19:42 +0100

    Tweaking allocations to summary accounts

--- luca/metadata.py
+++ luca/metadata.py
@@ -7,7 +7,7 @@ Information describing the project.
 package = 'luca'
 project = "Python Trial Balance Accounting"
 project_no_spaces = project.replace(' ', '')
-version = '0.0.141'
+version = '0.0.142'
 description = 'A pythonic interface Accounting Journal Entries and .'
 authors = ['drummonds']
 authors_string = ', '.join(authors)
