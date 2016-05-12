e211f6716e12274a139435cc192fab82ced60907
Humphrey <hum3@drummond.info>
2016-05-12 09:10:26 +0100

    bugfixing SLF_Mgmt_BS Making list sums more pythonic

--- luca/page_slf_mgmt.py
+++ luca/page_slf_mgmt.py
@@ -85,7 +85,8 @@ class SLF_Mgmt_BS(ExcelReportPage):
         xlb.write_bs_sum(ws, net_current_assets, 'NET CURRENT ASSETS', gap=2)
         xlb.write_bs_block(ws, long_term_liabilities, [2103], 'CREDITORS DUE AFTER MORE THAN 1 YEAR',
                             sub_total=True, sign=-1)
-        total_net_assets = [fa + nca - ltl for nca, ltl in zip(fixed_assets, net_current_assets, long_term_liabilities)]
+        total_net_assets = [fa + nca - ltl for fa, nca, ltl in
+                            zip(fixed_assets, net_current_assets, long_term_liabilities)]
         xlb.write_bs_sum(ws, total_net_assets, 'TOTAL NET ASSETS', gap = 3)
         # Owners equity side of balance sheet
         xlb.write_bs_block(ws, owners_equity, [2120, 2125, 2126], "SHAREHOLDERS' FUNDS", sign=-1)
