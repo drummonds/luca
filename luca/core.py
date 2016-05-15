0ffec43d63a383956b8648fad50ebd055b108791
Humphrey Drummond <hum3@drummond.info>
2016-05-15 22:30:08 +0100

    Bug fixing Drummonds COA.  Extending to proift and loss, before tax, after tax and afte divdidends

--- luca/core.py
+++ luca/core.py
@@ -17,9 +17,10 @@ DRUMMONDS_TO_FY_SUMMARY = {
     50: (5000, 5100),  # Turnover
     60: (6000, 6100, 6200, 7010, 7500),  # Cost of sales
     80: (7000, 7001, 7002, 7100, 7205, 7300, 7700, 8000, 8001, 8002, 8003, 8005, 8006, 8007,
-         8008, 8009, 8010, 8011, 8012, 8013, 8014, 8017, 8018, 8019, 8020, 8021, 8100, 8300, 8900),
-    # Administrative Expenses
+         8008, 8009, 8010, 8011, 8012, 8013, 8014,
+         8017, 8018, 8019, 8020, 8021, 8100, 8300, 8900),  # Administrative Expenses
     91: (9100, 9500, 9510),  # Tax on(loss)/profit on ordinary activities
+    92: (9200),  # Dividends
     }
 
 DRUMMONDS_TO_FY_DETAIL = {
@@ -48,8 +49,8 @@ DRUMMONDS_TO_FY_DETAIL = {
     835: (8100, ),  # Legal and professional Fees
     840: (8010, ),  # Travel and subsistence
     890: (7000, ),  # Bank charges
-    # Administrative Expenses
     910: (9100, 9500, 9510),  # Tax on(loss)/profit on ordinary activities
+    920: (9200),  # Dividends
     }
 
 SLF_MA_TO_FY_SUMMARY = {
